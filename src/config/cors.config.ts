c475814c14e396c1000164a3adba215cf5d7eb18
fabian-michael <fabian.michael9@icloud.com>
2024-02-16 22:08:19 +0100

    trim trailing slashes in cors config

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,6 +1,7 @@
+import { trimEndCurried } from "../utils";
 import { PAYLOAD_PUBLIC_FRONTEND_BASE_URL } from "./env.public";
 
 export const corsConfig = (): string[] => [
     'http://localhost:5173',
     PAYLOAD_PUBLIC_FRONTEND_BASE_URL,
-].filter(Boolean);
+].filter(Boolean).map(trimEndCurried('/'));
--- src/utils.ts
+++ src/utils.ts
@@ -10,6 +10,8 @@ export function trim(str: string, char: string): string {
     return trimStart(trimEnd(str, char), char);
 }
 
+export const trimEndCurried = (char: string) => (str: string) => trimEnd(str, char);
+export const trimStartCurried = (char: string) => (str: string) => trimStart(str, char);
 export const trimCurried = (char: string) => (str: string) => trim(str, char);
 
 export function buildUrl(parts: string[], trailingSlash?: boolean): string {
