79dbe335c24406ab1dce0a1cfc1a270e95661cee
fabian-michael <fabian.michael9@icloud.com>
2024-11-05 11:27:54 +0100

    add email config

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,22 @@ app.get('/', (_, res) => {
 const start = async () => {
   // Initialize Payload
   await payload.init({
-    secret: process.env.PAYLOAD_SECRET,
+    secret: process.env.PAYLOAD_SECRET!,
     express: app,
+    email: {
+      fromAddress: process.env.PAYLOAD_EMAIL_FROM_ADDRESS!,
+      fromName: process.env.PAYLOAD_EMAIL_FROM_NAME!,
+      transportOptions: {
+        host: process.env.SMTP_HOST!,
+        port: parseInt(process.env.SMTP_PORT!, 10),
+        secure: parseInt(process.env.SMTP_PORT!, 10) === 465,
+        requireTLS: process.env.NODE_ENV === 'production',
+        auth: {
+          user: process.env.SMTP_USER!,
+          pass: process.env.SMTP_PASS!,
+        }
+      }
+    },
     onInit: async () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
     },
